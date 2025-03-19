04fc92a176192950dce72f6ea1f5af640705a36c
Esdras0106 <esdras.miranda@ba.estudante.senai.br>
2025-03-19 19:58:12 +0000

    salvar

--- Atividades/18.py
+++ Atividades/18.py
@@ -1,28 +1,48 @@
 import os
-os.system("clear")
+os.system("clear || cls")
 
 nome_do_empregado = (input("Nome do empregado: "))
 idade = int(input("Idade do empregado: "))
 tempo_trabalhado = int(input("Tempo de Contribuição: "))
 codigo = int(input("Informe o seu código: "))
 
-match idade:
-    case 65 :    
+# <<<<<<< HEAD
+# match idade:
+#     case 65 :    
         
-        if tempo_trabalhado >= 30:
-            print("Requerer aposentadoria.")
-        else:
-            print("Não requerer aposentadoria.")
+#         if tempo_trabalhado >= 30:
+#             print("Requerer aposentadoria.")
+# =======
+# match idade:    
+    
+#     case 65 :
+#         if idade >=65:
+#              print("Requerer aposentadoria.")
+# >>>>>>> b45e68d (salvar)
+#         else:
+#              print("Não Requerer aposentadoria.")
+#     case _:
+# <<<<<<< HEAD
+#             if idade <65:
+#                 print("Não requerer aposentadoria.")
+#             elif idade >=65:
+#                 print("Requerer aposentadoria.")
+#             else:
+#                 print("Não requerer aposentadoria")
             
-    case _:
-            if idade <65:
-                print("Não requerer aposentadoria.")
-            elif idade >=65:
-                print("Requerer aposentadoria.")
-            else:
-                print("Não requerer aposentadoria")
-            
-print(f"\nNome do empregado: {nome_do_empregado}")
-print(f"\nIdade: {idade}")
-print(f"\nTempo de Contribuição: {tempo_trabalhado}")
-print(f"\nCódigo do empregado: {codigo}")
+# =======
+#             print("Não requerer aposentadoria.")
+ 
+# match tempo_trabalhado:
+
+#      case 30:
+#           if tempo_trabalhado >=30:
+#                print("Requerer aposentadoria.")
+#           else:
+#               print("Não requerer aposentadoria.")
+
+# >>>>>>> b45e68d (salvar)
+# print(f"\nNome do empregado: {nome_do_empregado}")
+# print(f"\nIdade: {idade}")
+# print(f"\nTempo de Contribuição: {tempo_trabalhado}")
+# print(f"\nCódigo do empregado: {codigo}")
