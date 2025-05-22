797569b95780593ee74cfbc926daecf0de6eb8ba
Esdras0106 <esdras.miranda@ba.estudante.senai.br>
2025-05-22 16:13:53 -0300

    Add files via upload

---
+++ javascript/atividade1.js
@@ -0,0 +1,10 @@
+ const readlineSync = require ('readline-sync')
+
+ let numero = parseInt(readlineSync.question("Digite um numero para a tabela:"))
+
+ console.log(`Tabela do numero ${numero}`)
+
+ for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`)
+ }
---
+++ javascript/laco_de_repeticao.js
@@ -0,0 +1,10 @@
+// Laço de Repetição
+
+// let i = 1 (Variável i começa com valor 1)
+// i <= 5 (Irá repertir até 5)
+// i ++ (adiciona 1 ao valor da variável i)
+// i += 1
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
