5404f683ddaefd2ef027c9ce67051407a83a63d9
Esdras0106 <esdras.miranda@ba.estudante.senai.br>
2025-05-26 16:45:20 -0300

    Add files via upload

---
+++ javascript/funcao/1.js
@@ -0,0 +1,38 @@
+// Criando função.
+
+function somar(a, b){
+    return a + b
+}
+
+function subtrair(a, b){
+    return a - b
+}
+
+function multiplicar(a, b){
+    return a * b
+}
+
+
+function dividir(a, b){
+    return a / b
+}
+
+// Chamando a função.
+// Adicionar o resultado da função numa constante.
+const soma = somar(6,3)
+const subtracao = subtrair(6,3)
+const multiplicacao = multiplicar(6,3)
+const divisao = dividir(6,3)
+
+
+
+
+
+
+
+// Mostrar o conteúdo da constante.
+console.log(`Soma: ${soma}`)
+console.log(`Subtracao: ${subtracao}`)
+console.log(`Multiplicacao: ${multiplicacao}`)
+console.log(`Divisao: ${divisao}`)
+
